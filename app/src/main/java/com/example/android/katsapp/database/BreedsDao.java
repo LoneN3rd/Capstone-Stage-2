e80e9a0d9ade45175f651f6ab1539cea66776342
Mercy Mutuku <mercymutuku19@gmail.com>
2019-04-21 20:37:30 +0300

    first commit

---
+++ app/src/main/java/com/example/android/katsapp/database/BreedsDao.java
@@ -0,0 +1,24 @@
+package com.example.android.katsapp.database;
+
+
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
+public interface BreedsDao {
+
+    @Query("SELECT * FROM breeds")
+    List<FavoriteBreeds> getAll();
+
+    @Query("SELECT * FROM breeds WHERE breed_id = :id")
+    FavoriteBreeds getBreedById(String id);
+
+    // insert single record
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insertBreed(FavoriteBreeds favoriteBreeds);
+
+    @Query("DELETE FROM breeds WHERE breed_id = :id")
+    void delete(String id);
+}
