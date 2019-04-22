06402a2e0bcca69401edcccb2103cb222d8dceee
Mercy Mutuku <mercymutuku19@gmail.com>
2019-04-22 23:20:54 +0300

    change getBreedById return type

--- app/src/main/java/com/example/android/katsapp/database/BreedsDao.java
+++ app/src/main/java/com/example/android/katsapp/database/BreedsDao.java
@@ -1,7 +1,6 @@
 package com.example.android.katsapp.database;
 
 
-import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.OnConflictStrategy;
@@ -13,7 +12,7 @@ import java.util.List;
 public interface BreedsDao {
 
     @Query("SELECT * FROM breeds ORDER BY breed_name ASC")
-    LiveData<List<FavoriteBreeds>> getAll();
+    List<FavoriteBreeds> getAll();
 
     @Query("SELECT * FROM breeds WHERE breed_id = :id")
     FavoriteBreeds getBreedById(String id);
