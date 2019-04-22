7ca8a8c2fd57a996db661b44c60be4fbe47873e2
Mercy Mutuku <mercymutuku19@gmail.com>
2019-04-22 23:21:19 +0300

    update version number

--- app/src/main/java/com/example/android/katsapp/database/BreedsDatabase.java
+++ app/src/main/java/com/example/android/katsapp/database/BreedsDatabase.java
@@ -3,7 +3,7 @@ package com.example.android.katsapp.database;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.RoomDatabase;
 
-@Database(entities = {FavoriteBreeds.class}, version = 1, exportSchema = false)
+@Database(entities = {FavoriteBreeds.class}, version = 2, exportSchema = false)
 public abstract class BreedsDatabase extends RoomDatabase {
 
     public abstract BreedsDao breedsDao();
