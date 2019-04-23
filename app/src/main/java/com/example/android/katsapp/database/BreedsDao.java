462a118121809ec25abfff2279db76ce508a410e
Mercy Mutuku <mercymutuku19@gmail.com>
2019-04-24 00:38:35 +0300

    Update getBreedById() and getAll() return types

--- app/src/main/java/com/example/android/katsapp/database/BreedsDao.java
+++ app/src/main/java/com/example/android/katsapp/database/BreedsDao.java
@@ -1,6 +1,7 @@
 package com.example.android.katsapp.database;
 
 
+import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.OnConflictStrategy;
@@ -12,10 +13,10 @@ import java.util.List;
 public interface BreedsDao {
 
     @Query("SELECT * FROM breeds ORDER BY breed_name ASC")
-    List<FavoriteBreeds> getAll();
+    LiveData<List<FavoriteBreeds>> getAll();
 
     @Query("SELECT * FROM breeds WHERE breed_id = :id")
-    FavoriteBreeds getBreedById(String id);
+    LiveData<FavoriteBreeds> getBreedById(String id);
 
     // insert single record
     @Insert(onConflict = OnConflictStrategy.IGNORE)
