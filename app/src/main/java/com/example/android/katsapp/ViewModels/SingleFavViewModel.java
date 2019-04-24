8a5addfdeba2cbd683697f444e966a40d3795fe7
Mercy Mutuku <mercymutuku19@gmail.com>
2019-04-25 00:24:55 +0300

    Update SingleFavViewModel

--- app/src/main/java/com/example/android/katsapp/ViewModels/SingleFavViewModel.java
+++ app/src/main/java/com/example/android/katsapp/ViewModels/SingleFavViewModel.java
@@ -9,13 +9,19 @@ import com.example.android.katsapp.database.FavoriteBreeds;
 
 public class SingleFavViewModel extends ViewModel {
 
+    // Constant for logging
+    private static final String LOG_TAG = FavoritesViewModel.class.getSimpleName();
     private LiveData<FavoriteBreeds> favorite;
 
+    /*
+    Create a constructor where you call loadEntryById of the entryDao to initialize
+    the favorite variable
+     */
     public SingleFavViewModel(BreedsDatabase database, String breedId) {
         favorite = database.breedsDao().getBreedById(breedId);
     }
 
-    // Create a getter for the entry variable
+    // Create a getter for the favorite variable
     public LiveData<FavoriteBreeds> getFavorite() {
         return favorite;
     }
